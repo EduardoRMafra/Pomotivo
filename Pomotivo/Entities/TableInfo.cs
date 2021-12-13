9fea84507b7facd1652ac264ddf332a42e147e25
Eduardo Mafra <edu.r.mafra@gmail.com>
2021-12-13 10:27:15 -0300

    Adição de valores padrão para o tempo de tarefa e para as pausas. Criação da classe TableInfo com as colunas da tabela.

---
+++ Pomotivo/Entities/TableInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Pomotivo.Entities
+{
+    static class TableInfo
+    {
+        public static DataTable dt = new DataTable();
+
+        public static void TableColumns()
+        {
+            dt.Columns.Add("Sequence", typeof(int));
+            dt.Columns.Add("Task", typeof(string));
+            dt.Columns.Add("Quantity", typeof(int));
+            dt.Columns.Add("Time", typeof(double));
+        }
+    }
+}
--- Pomotivo/Form1.cs
+++ Pomotivo/Form1.cs
@@ -7,15 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Pomotivo.Entities;
 
 namespace Pomotivo
 {
     public partial class Form1 : Form
     {
+        string taskDefault = "25";
+        string smallBreakDefault = "5";
+        string longBreakDefault = "30";
         public Form1()
         {
             InitializeComponent();
+            InitializeProgram();
         }
 
+        public void InitializeProgram()
+        {
+            StartTable();
+        }
+        public void StartTable()
+        {
+            //atribui as colunas que a tabela deve ter
+            TableInfo.TableColumns();
+            //exibe a tabela no dataGrid
+            dataGridView1.DataSource = TableInfo.dt;
+        }
+
+        
     }
 }
